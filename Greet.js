7372b4a45b8fb0aceeb3f0ba301901bdff184638
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-22 15:42:21 +0300

    destructuring props in functional components

---
+++ Greet.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Greet=({name,heroName})=>{
+   // console.log({name,heroName})
+return(
+    <div>
+        <h1> Hello {name} a.k.a {heroName}</h1>
+    </div>
+)}
+export default Greet
+
