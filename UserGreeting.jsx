fda6667ab5cd2c869b470e2b9a648d91918be687
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-23 12:26:58 +0300

    Conditional rendering using if else

---
+++ UserGreeting.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+ class UserGreeting extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isLoggedIn:true
+    }
+  }
+  
+    render() {
+if(this.state.isLoggedIn){
+    return(
+        <div>Welcome Munyiri</div>
+    )
+}
+else{
+    return(
+        <div>Welcome Guest</div>
+    )
+}
+   // return (
+
+    //  <div>
+     //   <div>Welcome Munyiri</div>
+     //   <div>Welcome Guest</div>
+    //  </div>
+    //)
+  }
+}
+
+export default UserGreeting
