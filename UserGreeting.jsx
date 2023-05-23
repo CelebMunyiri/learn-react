1f35a70195db020a051a2640925c4f5bfb6100a4
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-23 12:43:27 +0300

    Conditional rendering by ternary and element varia

--- UserGreeting.jsx
+++ UserGreeting.jsx
@@ -6,20 +6,39 @@ import React, { Component } from 'react'
   
     this.state = {
        isLoggedIn:true
+
     }
   }
   
     render() {
-if(this.state.isLoggedIn){
-    return(
-        <div>Welcome Munyiri</div>
-    )
-}
-else{
-    return(
-        <div>Welcome Guest</div>
-    )
-}
+        //condition rendering using ternary conditional operator starts here
+        return(
+            this.state.isLoggedIn ? 
+            <div>Welcome Munyiri abdul</div>:
+            <div>Welcome the guest</div>
+        )
+        //condtional rendering using element variables starts below
+        //let message
+        //if(this.state.isLoggedIn){
+          //  message=<div>Welcome Guests</div>
+        //}
+        //else{
+          //  message=<div>Welcome Munyirii</div>
+        //}
+        //return(
+          //  <div>{message}</div>
+        //)
+        //if else starts below
+//if(this.state.isLoggedIn){
+   // return(
+    //    <div>Welcome Munyiri</div>
+   // )
+//}
+//else{
+    //return(
+      //  <div>Welcome Guest</div>
+    //)
+//}
    // return (
 
     //  <div>
