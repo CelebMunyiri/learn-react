6371bc8707299a9b0c18a2c4169c2e94408c5b7e
David Munyiri <81283410+CelebMunyiri@users.noreply.github.com>
2023-05-24 11:35:38 +0300

    Updated app file

---
+++ App.js
@@ -0,0 +1,55 @@
+
+import './App.css';
+//import Stylesheet from './components/Stylesheet';
+//import NamesList from './components/NamesList';
+//import List from './components/List';
+//import UserGreeting from './components/UserGreeting';
+//import Count from './Count';
+//import New from './components/New'
+//import Parent from './components/Parent';
+//import Eventbind from './components/Eventbind';
+//import Classclick from './components/Classclick';
+//import State from './components/State';
+//import StateandsetState from './components/StateandsetState';
+//import Greet from './components/Greet.js'
+//import Hello from './components/Hello.js'
+//import Welcome from './components/Welcome.js'
+//import Counter from './components/Counter';
+// import Functionclick from './components/Functionclick';
+//import './appStyles.css'
+//import styles from './appStyles.module.css'
+
+//inlince components are scoped by default
+//same style applies to all child components and parent component
+//this can result to styling conflicts making browser confused on which style to use
+//import Inline from './components/Inline';
+const App=()=> {
+
+  return (
+    <div className="App">
+  {/*    <h1 className='error'>Error</h1>*/}
+{/*<h1 className={styles.success}>Success</h1>*/}
+  {/*<Inline/> */}    
+      {/*<Stylesheet primary={true} />*/}
+{/*<NamesList/>*/}
+      {/*<List/>*/}
+      {/*<UserGreeting/>*/}
+     {/*<Parent/> */} 
+      {/*<Eventbind/>*/}
+     {/*<Classclick/>*/ } 
+      {/*<Functionclick/>*/}
+
+
+      {/*<StateandsetState/>*/}
+     {/*<State/> */} 
+     {/*<New/>*/}
+     {/*<Counter/> */}
+{/*<Welcome name='Dawud' heroName='Abdul'/>*/}
+     {/*<Count/>*/}
+     {/*<Greet name='Munyiri' heroName='Mahubali'/>*/}
+      
+    </div>
+  );
+}
+
+export default App;
